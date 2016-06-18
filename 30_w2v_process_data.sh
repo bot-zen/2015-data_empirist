6d388d1e443c5a24892a1b44e127fb2e88c1a81a
egon w. stemle <egon.stemle@unitn.it>
2016-06-18 23:27:19 +0200

    30_w2v_process_data.sh - change input file to ARG1; add opt to skip building vocab

--- 30_w2v_process_data.sh
+++ 30_w2v_process_data.sh
@@ -12,7 +12,7 @@ DATA_OUT_DIR="./word2vec"
 W2V_THREADS=${W2V_THREADS:-4}
 W2V_DEBUG=${W2V_DEBUG:-2}
 
-[ -f "$DATA_IN" ] || ( echo "missing \$DATA_IN: ${DATA_IN}"; exit 1)
+[ -z $DATA_NAME ] && ( echo "missing \$DATA_NAME"; exit 1)
 [ -z $W2V_SIZE ] && (echo "missing: \$W2V_SIZE"; exit 1)            # 500
 [ -z $W2V_WINDOW ] && (echo "missing: \$W2V_WINDOW"; exit 1)        #  10
 [ -z $W2V_SAMPLE ] && (echo "missing: \$W2V_SAMPLE"; exit 1)        #  10
@@ -23,16 +23,17 @@ W2V_DEBUG=${W2V_DEBUG:-2}
 [ -z $W2V_ALPHA ] && (echo "missing: \$W2V_ALPHA"; exit 1)          #
 [ -z $W2V_CBOW ] && (echo "missing: \$W2V_CBOW"; exit 1)            #
 
-DATA_IN_DIR=$(dirname "$DATA_IN")
-DATA_IN_FN=$(basename "$DATA_IN")
-
-DATA_OUT_FN=$(basename "$DATA_IN" .txt)-size${W2V_SIZE}-window${W2V_WINDOW}-sample${W2V_SAMPLE}-hs${W2V_HS}-neg${W2V_NEGATIVE}-iter${W2V_ITER}-mincnt${W2V_MINCOUNT}-alpha${W2V_ALPHA}-cbow${W2V_CBOW}
+DATA_CLEAN_VER=${DATA_CLEAN_VER:-1}
+DATA_OUT_FN="${DATA_CLEAN_VER}_${DATA_NAME}-size${W2V_SIZE}-window${W2V_WINDOW}-sample${W2V_SAMPLE}-hs${W2V_HS}-neg${W2V_NEGATIVE}-iter${W2V_ITER}-mincnt${W2V_MINCOUNT}-alpha${W2V_ALPHA}-cbow${W2V_CBOW}"
+echo "output file name: ${DATA_OUT_FN}"
 
 [ -f "$DATA_OUT_DIR"/"$DATA_OUT_FN".vec ] && ( echo "output already exists: ${DATA_OUT_DIR}/${DATA_OUT_FN}.vec"; exit 1)
 
 mkdir -p word2vec
+[ -z $W2V_READ_VOCAB ] && 
+{
 ./tmp/word2vec.svn/word2vec \
-    -train "${DATA_IN_DIR}"/"${DATA_IN_FN}" \
+    -train "$1" \
     -output "${DATA_OUT_DIR}"/"${DATA_OUT_FN}".vec \
     -size $W2V_SIZE \
     -window $W2V_WINDOW \
@@ -46,3 +47,19 @@ mkdir -p word2vec
     -threads $W2V_THREADS \
     -debug $W2V_DEBUG \
     -save-vocab "${DATA_OUT_DIR}"/"${DATA_OUT_FN}".voc
+} 
+./tmp/word2vec.svn/word2vec \
+    -train "$1" \
+    -output "${DATA_OUT_DIR}"/"${DATA_OUT_FN}".vec \
+    -size $W2V_SIZE \
+    -window $W2V_WINDOW \
+    -sample $W2V_SAMPLE \
+    -hs $W2V_HS \
+    -negative $W2V_NEGATIVE \
+    -iter $W2V_ITER \
+    -min-count $W2V_MINCOUNT \
+    -alpha $W2V_ALPHA \
+    -cbow $W2V_CBOW \
+    -threads $W2V_THREADS \
+    -debug $W2V_DEBUG \
+    -read-vocab "${DATA_OUT_DIR}"/"${DATA_OUT_FN}".voc
