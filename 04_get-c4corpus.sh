511292aec21844b362b1a48c730bcd15b5e8146e
egon w. stemle <egon.stemle@eurac.edu>
2016-10-26 10:53:54 +0200

    04_get-c4corpus.sh - mv download dir to tmp

--- 04_get-c4corpus.sh
+++ 04_get-c4corpus.sh
@@ -5,7 +5,7 @@ which aws || { echo "You need to installi the 'Universal Command Line Environmen
 aws s3 ls s3://ukp-research-data/c4corpus/CC-MAIN-2015-48/cc-final-2015-48/ | grep 'Lang_de'
 
 aws s3 sync \
-    s3://ukp-research-data/c4corpus/CC-MAIN-2015-48/cc-final-2015-48/ c4corpus-2015-11/ \
+    s3://ukp-research-data/c4corpus/CC-MAIN-2015-48/cc-final-2015-48/ tmp/c4corpus-final-2015-48/ \
     --exclude '*' \
     --include '*Lic_by*Lang_de*' \
     --include '*Lic_public*Lang_de*' \
@@ -15,7 +15,7 @@ shopt -s nocasematch
 [[ ${C4CORPUS_BIGFILES} == "true" ]] \
 && \
 aws s3 sync \
-    s3://ukp-research-data/c4corpus/CC-MAIN-2015-48/cc-final-2015-48/ c4corpus-2015-11/ \
+    s3://ukp-research-data/c4corpus/CC-MAIN-2015-48/cc-final-2015-48/ tmp/c4corpus-final-2015-48 \
     --exclude '*' \
     --include '*Lic_none*Lang_de*' \
 || echo "C4CORPUS_BIGFILES not true: not downloading big files."
