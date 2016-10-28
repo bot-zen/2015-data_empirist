e85df1533d95a0635dba317d6f1986babfc91114
egon w. stemle <egon.stemle@eurac.edu>
2016-10-28 16:04:09 +0200

    11_w2v_prep-empirist.sh,13_w2v_prep-de.wikipedia.org.sh - avoid construction of pre-w2v-wiki file

--- 11_w2v_prep-empirist.sh
+++ 11_w2v_prep-empirist.sh
@@ -7,10 +7,10 @@
 set -e
 . 10_w2v_prep_data
 
-[ -e ./tmp/pre-w2v-${CLEAN_VERSION}_empirist.txt ] || \
+[ -e ./tmp/pre-w2v_empirist.txt ] || \
 cat <(cat ./empirist/empirist_training_{cmc,web}/tokenized/*.txt \
       | sed -e 's#^<posting.*#<s>#' -e 's#<article.*#<s>#' -e 's#^$#</s>#') \
-    > ./tmp/pre-w2v-${CLEAN_VERSION}_empirist.txt
+    > ./tmp/pre-w2v_empirist.txt
 
 [ -e ./tmp/w2v-${CLEAN_VERSION}_empirist.txt ] || \
 clean < ./tmp/pre-w2v-${CLEAN_VERSION}_empirist.txt > ./tmp/w2v-${CLEAN_VERSION}_empirist.txt
--- 13_w2v_prep-de.wikipedia.org.sh
+++ 13_w2v_prep-de.wikipedia.org.sh
@@ -7,9 +7,12 @@
 set -e
 . 10_w2v_prep_data
 
-[ -e ./tmp/pre-w2v-${CLEAN_VERSION}_de.wikipedia.org.txt ] || \
-cat <(bzip2 -c -d ./tmp/de.wikipedia.org/*.tt.bz2) \
-    > ./tmp/pre-w2v-${CLEAN_VERSION}_de.wikipedia.org.txt
+[ -e ./tmp/pre-w2v_de.wikipedia.readme ] || \
+    cat > ./tmp/pre-w2v_de.wikipedia.readme << END
+cat <(bzip2 -c -d ./de.wikipedia.org/*.tt.bz2) \
+    | bzip2 -c -9 ./pre-w2v_de.wikipedia.org.txt.bz2
+END
 
 [ -e ./tmp/w2v-${CLEAN_VERSION}_de.wikipedia.org.txt ] || \
-clean < ./tmp/pre-w2v-${CLEAN_VERSION}_de.wikipedia.org.txt > ./tmp/w2v-${CLEAN_VERSION}_de.wikipedia.org.txt
+    clean <(bzip2 -c -d ./tmp/de.wikipedia.org/*.tt.bz2)  \
+    > ./tmp/w2v-${CLEAN_VERSION}_de.wikipedia.org.txt
